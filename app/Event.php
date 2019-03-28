db2c7509593edf3c6580bd0a6d2ea051e3d1353d
Monticelli Romain <rmm.monticelli@gmail.com>
2019-03-28 15:53:19 +0100

    basic crud for events

--- app/Event.php
+++ app/Event.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    protected $fillable = [
+        'name',
+        'date',
+        'description'
+    ];
+    
     public function users(){
 
         return $this->belongsToMany(User::class, 'user_event');
