cb2d935ff040bb09b7f013d96734dab35ae258f6
matthieu <matthieu.jasselette@gmail.com>
2019-03-28 14:33:13 +0100

    add react.fragment in eventlist

--- resources/js/components/views/EventList.js
+++ resources/js/components/views/EventList.js
@@ -5,15 +5,17 @@ import { Button } from 'react-bootstrap';
 export default class EventList extends Component {
   render() {
     return(
-      <div>
-        <p>Liste des évènements</p>
-        <Link to={"/event-create"}>
-        <Button variant="secondary">Create Event</Button>
-        </Link>
-        <Link to={"/event-display"}>
-        <Button variant="secondary">More info</Button>
-        </Link>
-      </div>
+      <React.Fragment>
+        <div>
+          <p>Liste des évènements</p>
+          <Link to={"/event-create"}>
+          <Button variant="secondary">Create Event</Button>
+          </Link>
+          <Link to={"/event-display"}>
+          <Button variant="secondary">More info</Button>
+          </Link>
+        </div>
+      </React.Fragment>
     )
   }
 }
