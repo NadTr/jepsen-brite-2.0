b544b2074620c998be1cbd5c86151f2f3fe83e06
matthieu <matthieu.jasselette@gmail.com>
2019-03-29 14:01:43 +0100

    change target api

--- resources/js/components/Api.js
+++ resources/js/components/Api.js
@@ -1,37 +1,37 @@
 import axios from 'axios';
 
 //crud
-export const createEvent = (obj) => {
-  axios
-    .post('localhost:8000/api/events/create', obj)
-    // .then(this.props.history.push('/'))
-    .catch(err => console.log(err))
-}
+// export const createEvent = (obj) => {
+//   axios
+//     .post('localhost:8000/api/events/create', obj)
+//     // .then(this.props.history.push('/'))
+//     .catch(err => console.log(err))
+// }
 
 export const getAllEvents = () => {
   axios
-    .get('localhost:8000/api/events')
+    .get('https://api.punkapi.com/v2/beers')
     .then(result => {events : result.data})
     .catch(err => console.log(err))
 }
 
 export const getOneEvent = (eventId) => {
   axios
-    .get('localhost:8000/api/events/'+eventId)
+    .get('https://api.punkapi.com/v2/beers/'+eventId)
     .then(result => {event : result.data})
     .catch(err => console.log(err))
 }
 
-export const editEvent = (obj) => {
-  axios
-    .put('localhost:8000/api/events/', obj)
-    // .then(this.props.history.push('/'))
-    .catch(err => console.log(err))
-}
+// export const editEvent = (obj) => {
+//   axios
+//     .put('localhost:8000/api/events/', obj)
+//     // .then(this.props.history.push('/'))
+//     .catch(err => console.log(err))
+// }
 
-export const deleteEvent = (eventId) => {
-  axios
-  .delete('localhost:8000/api/events/'+eventId)
-  // .then(this.props.history.push('/'))
-  .catch(err => console.log(err))
-}
+// export const deleteEvent = (eventId) => {
+//   axios
+//   .delete('localhost:8000/api/events/'+eventId)
+//   // .then(this.props.history.push('/'))
+//   .catch(err => console.log(err))
+// }
