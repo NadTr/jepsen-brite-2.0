2f36872f42e2192cc3302eeb12ee0bed65bb0ccf
Monticelli Romain <rmm.monticelli@gmail.com>
2019-04-01 11:34:37 +0200

    database heroku maj

--- config/database.php
+++ config/database.php
@@ -69,19 +69,6 @@ return [
             'prefix'   => '',
             'schema'   => 'public',
         ],
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'becode'),
-            'username' => env('DB_USERNAME', 'becode'),
-            'password' => env('DB_PASSWORD', 'becode'),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
