f3c74e0697584c4aaf0f18cce7f6b577141df0ab
Monticelli Romain <rmm.monticelli@gmail.com>
2019-04-01 11:32:50 +0200

    database heroku maj

--- config/database.php
+++ config/database.php
@@ -60,6 +60,16 @@ return [
         ],
 
         'pgsql' => [
+            'driver'   => 'pgsql',
+            'host'     => parse_url(getenv("DATABASE_URL"))["host"],
+            'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+        ],
+        'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
