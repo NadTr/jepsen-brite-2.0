e5282759a68b42e971d37c1606f13711aa4f6bdd
Monticelli Romain <rmm.monticelli@gmail.com>
2019-04-03 14:29:01 +0200

    fixing register

--- routes/api.php
+++ routes/api.php
@@ -27,8 +27,8 @@ Route::middleware('auth:api')->group(function () {
     Route::get('/events/{offset}/{limit}', 'EventController@pastEvent')->name('events.pastevent');
 });
 
-Route::post('/register','Auth\RegisterController@register');
+//Route::post('/register','Auth\RegisterController@register');
 
 Route::post('/login','ApiAuthController@login')->name('events.login');
 Route::get('/user','ApiAuthController@user')->name('app.user');
-
+Auth::routes();
