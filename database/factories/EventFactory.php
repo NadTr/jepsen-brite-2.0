32fe04c5d9cd58ea768dad35f531116cd223b335
marween <annemagali.saintgeorges@gmail.com>
2019-04-04 14:01:17 +0200

    seed ok

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -7,6 +7,6 @@ $factory->define(App\Event::class, function (Faker $faker) {
         'name'        => $faker->name(),
         'date'        => $faker->date($format = 'Y-m-d', $max = 'now'),
         'description' => $faker->name(),
-        'author' => $faker->integer()
+        'author'      => factory ('App\User')->create()->id
     ];
 });
