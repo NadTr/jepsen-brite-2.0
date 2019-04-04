f7056ef06f21e3acc26e411237200abc7d034ecd
grosjeanstephanie <grosjean.stephaniep@gmail.com>
2019-04-04 10:58:12 +0200

    add font-family in app.scss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>EventFood</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Bree+Serif|Gloria+Hallelujah" rel="stylesheet">
 
         <link href="./css/app.css" rel="stylesheet">
         <!-- Styles -->
