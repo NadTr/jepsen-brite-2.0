9201f61d96b4c93be5701a26ea48381bc5611273
grosjeanstephanie <grosjean.stephaniep@gmail.com>
2019-04-04 09:47:08 +0200

    pas push

--- resources/js/components/UserRegisterContainer.js
+++ resources/js/components/UserRegisterContainer.js
@@ -2,12 +2,75 @@ import React, { Component } from 'react';
 
 //import components
 import UserRegister from './views/UserRegister';
+import { registerUser } from './Api';
 
 
 export default class UserRegisterContainer extends Component{
+    constructor(props){
+    super(props);
+    
+        this.onChangeEmailAdress = this.onChangeEmailAdress.bind(this);
+        this.onChangePassword = this.onChangePassword.bind(this);
+        this.onChangeName = this.onChangeName.bind(this);
+        this.onChangePseudo = this.onChangePseudo.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    
+        this.state = {
+          email: "",
+          name : "",
+          pseudo: "",
+          password: ""
+        }
+    }
+    
+    onChangeEmailAdress(input) {
+      console.log("changeEmail");
+      this.setState({
+        emailAdress: input.target.value
+      })
+    }
+    
+    onChangePassword(input) {
+      this.setState({
+        password: input.target.value
+      })
+    }
+    
+    onChangeName(input) {
+      console.log("changeName");
+      this.setState({
+        name: input.target.value
+      })
+    }
+    
+    onChangePseudo(input) {
+      this.setState({
+        pseudo: input.target.value
+      })
+    }
+    
+    onSubmit(data) {
+      console.log("1000");
+      data.preventDefault();
+      let obj = {
+        "email": this.state.emailAdress,
+        "name" : this.state.name,
+        "pseudo": this.state.pseudo,
+        "password": this.state.password
+      };
+      registerUser(JSON.stringify(obj));
+      console.log(obj);
+    }
+    
     render(){
         return(
-            <UserRegister />
+            <UserRegister
+            onChangeEmailAdress={this.onChangeEmailAdress}
+            onChangePassword={this.onChangePassword}
+            onChangeName={this.onChangeName}
+            onChangePseudo={this.onChangePseudo}
+            onSubmit={this.onSubmit}
+            />
         );
     }
 }
--- resources/js/components/views/UserRegister.js
+++ resources/js/components/views/UserRegister.js
@@ -5,36 +5,40 @@ import { Form } from 'react-bootstrap';
 
 export default class UserRegister extends Component{
     render(){
-        return(
-            <Form>
-                <Form.Group controlId="formBasicName">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control type="name" placeholder="Name" />
-                </Form.Group>
-                <Form.Group controlId="formBasicPseudo">
-                    <Form.Label>Pseudo</Form.Label>
-                    <Form.Control type="pseudo" placeholder="Pseudo" />
-                </Form.Group>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                    </Form.Text>
-                </Form.Group>
-
+    return(
+    <Form>
+    <Form.Group controlId="formBasicName">
+    <Form.Label>Name</Form.Label>
+    <Form.Control type="name" placeholder="Name" onChange={this.props.onChangeName}/>
+    </Form.Group>
+    <Form.Group controlId="formBasicPseudo">
+    <Form.Label>Pseudo</Form.Label>
+    <Form.Control type="pseudo" placeholder="Pseudo" onChange={this.props.onChangePseudo}/>
+    </Form.Group>
+    <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" onChange={this.props.onChangeEmailAdress}/>
+    <Form.Text className="text-muted">
+    We'll never share your email with anyone else.
+    </Form.Text>
+    </Form.Group>
+    
                 <Form.Group controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={this.props.onChangePassword}/>
                 </Form.Group>
                 <Form.Group controlId="formConfirmPassword">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
+                <Button variant="primary" type="submit" onClick={this.props.onSubmit}>
                     Submit
                 </Button>
             </Form>
         );
     }
 }
+    
+    
+    
+    
