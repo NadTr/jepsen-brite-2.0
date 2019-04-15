df0f62beb92e6e487c283098c7d4abfa81834ce7
Jeremy Scala <scalajeremy@gmail.com>
2019-04-15 09:19:18 +0200

    changing name

---
+++ resources/js/components/EventSearch.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class EventSearch extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+
+    }
+  }
+  render(){
+    return(
+      <>
+        <p></p>
+        <h5>This is the search pages</h5>
+      </>
+    )
+  }
+}
