6413b997c5b36576f689ba2bb7597a8a59e1bb4c
Jeremy Scala <scalajeremy@gmail.com>
2019-04-19 11:27:57 +0200

    first style, trying another

--- resources/js/components/views/CardLayout.js
+++ resources/js/components/views/CardLayout.js
@@ -30,26 +30,13 @@ export default class CardLayout extends Component {
 
     return(
       <>
-        <div className="card small-card" style={{ marginBottom: '0.5rem' }} >
-          <div className="card-body p-2 text-center d-flex justify-content-center flex-column">
-            <div className="bg-white">
-            <div className="card-title">
-              <Link to={"/event/"+this.props.event.id} style={{zIndex: '1', position: 'relative'}}>
-                <h3 style={{zIndex: '1', position: 'relative'}}>{this.props.event.event_title}</h3>
-              </Link>
-            </div>
-              <TextTruncate
-                     line={2}
-                     truncateText="…"
-                     text={this.props.event.event_description}
-                     textTruncateChild={<Link to={"/event/"+this.props.event.id} onClick={() => reload(this.props.event.id)}>Read more</Link>} />
-             <div className="card-text" style={{zIndex: '1', position: 'relative'}}>
-              <Moment format="DD MMM YYYY - H:mm">{this.props.event.date}</Moment>
-            </div>
-            </div>
-            <div id={"mediaHolder"+eventId}></div>
-          </div>
-        </div>
+        <figure className="media blue"><div className="media-holder" id={"mediaHolder"+eventId}></div>
+          <figcaption>
+            <h4>{this.props.event.event_title}</h4>
+          </figcaption>
+          <a href={"/event/"+this.props.event.id}></a>
+        </figure>
+
       </>
     )
   }
